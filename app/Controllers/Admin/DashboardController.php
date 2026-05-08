0cb6594fae187fff725bbae4b48f98d0d6f6d928
Francisco Kássio Lima Rodrigues Filho <frncisco.kassio@gmail.com>
2026-05-08 17:53:29 -0300

    feat(controller): criar o dashboardController do admin

---
+++ app/Controllers/Admin/DashboardController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Core\Auth;
+use App\Core\Controller;
+use App\Core\Permission;
+use App\Models\Departments\Department;
+use App\Models\Role\Role;
+use App\Models\Ticket\Ticket;
+use App\Models\User;
+
+class DashboardController extends Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct("App");
+
+        Auth::requirePermission(Permission::VIEW_MANAGER_DASHBOARD);
+    }
+
+    public function index(): void
+    {
+        Auth::requirePermission(Permission::VIEW_MANAGER_DASHBOARD);
+
+        $totalUsers = (new User())->totalUsers('ativo');
+        $totalRoles = (new Role())->totalRoles();
+        $totalDepartments = (new Department())->totalDepartments();
+        $totalOpenTickets = (new Ticket())->totalOpenTickets();
+
+
+            echo $this->view->render("admin/dashboard",
+                [
+                    "title" => "Dashboard | Admin" . APP_NAME,
+                    "totalUsers" => $totalUsers,
+                    "totalRoles" => $totalRoles,
+                    "totalDepartments" => $totalDepartments,
+                    "totalOpenTickets" => $totalOpenTickets,
+
+                ]);
+        }
+
+
+}
