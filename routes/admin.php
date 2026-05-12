c86cb75d52308aaaaf4c591e5d9592cb00fc8bc2
Francisco Kássio Lima Rodrigues Filho <frncisco.kassio@gmail.com>
2026-05-12 14:24:11 -0300

    feat(routes): adicionar rotas de perfis e permições no admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -55,3 +55,27 @@ $router->post("/departamentos/cadastrar", "Admin\\DepartmentController@store");
 $router->get("/departamentos/editar/{id}", "Admin\\DepartmentController@edit");
 $router->put("/departamentos/editar/{id}", "Admin\\DepartmentController@update");
 $router->delete("/departamentos/excluir/{id}", "Admin\\DepartmentController@destroy");
+
+
+/*
+|--------------------------------------------------------------------------
+| Rotas de perfis de acesso
+|--------------------------------------------------------------------------
+*/
+
+$router->get("/perfis", "Admin\\RoleController@index");
+$router->get("/perfis/cadastrar", "Admin\\RoleController@create");
+$router->post("/perfis/cadastrar", "Admin\\RoleController@store");
+$router->get("/perfis/editar/{id}", "Admin\\RoleController@edit");
+$router->put("/perfis/editar/{id}", "Admin\\RoleController@update");
+$router->delete("/perfis/excluir/{id}", "Admin\\RoleController@destroy");
+
+
+/*
+|--------------------------------------------------------------------------
+| Rotas de perfis de acesso
+|--------------------------------------------------------------------------
+*/
+
+$router->get("/perfis/{id}/permissoes", "Admin\\RolePermissionController@edit");
+$router->post("/perfis/{id}/permissoes", "Admin\\RolePermissionController@update");
