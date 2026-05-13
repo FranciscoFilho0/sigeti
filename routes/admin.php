ec07876c0cefe1bde72a365df007a6f290f44d1c
Francisco Kássio Lima Rodrigues Filho <frncisco.kassio@gmail.com>
2026-05-13 15:58:42 -0300

    refactor(routes): mover rotas de categorias e escolas para o admin

--- routes/admin.php
+++ routes/admin.php
@@ -56,3 +56,27 @@ $router->get("/departamentos/editar/{id}", "Admin\\DepartmentController@edit");
 $router->put("/departamentos/editar/{id}", "Admin\\DepartmentController@update");
 $router->delete("/departamentos/excluir/{id}", "Admin\\DepartmentController@destroy");
 
+/*
+|--------------------------------------------------------------------------
+| Rotas das categorias
+|--------------------------------------------------------------------------
+*/
+
+$router->get("/categorias", "Admin\\CategoryController@index");
+$router->get("/categorias/cadastrar", "Admin\\CategoryController@create");
+$router->post("/categorias/cadastrar", "Admin\\CategoryController@store");
+$router->get("/categorias/editar/{id}", "Admin\\CategoryController@edit");
+$router->put("/categorias/editar/{id}", "Admin\\CategoryController@update");
+$router->delete("categorias/excluir/{id}", "Admin\\CategoryController@destroy");
+/*
+|--------------------------------------------------------------------------
+| Rotas das escolas
+|--------------------------------------------------------------------------
+*/
+
+$router->get("/escolas", "Admin\\SchoolController@index");
+$router->get("/escolas/cadastrar", "Admin\\SchoolController@create");
+$router->post("/escolas/cadastrar", "Admin\\SchoolController@store");
+$router->get("/escolas/editar/{id}", "Admin\\SchoolController@edit");
+$router->put("/escolas/editar/{id}", "Admin\\SchoolController@update");
+$router->delete("escolas/excluir/{id}", "Admin\\SchoolController@destroy");
--- routes/web.php
+++ routes/web.php
@@ -26,30 +26,6 @@ require __DIR__ . "/profile.php";
 $router->group("/tecnico");
 $router->get("/dashboard", "Technician\\DashboardController@index");
 
-//rotas das categorias
-
-$router->get("/categorias", "Technician\\CategoryController@index");
-$router->get("/categorias/cadastrar", "Technician\\CategoryController@create");
-$router->post("/categorias/cadastrar", "Technician\\CategoryController@store");
-$router->get("/categorias/editar/{id}", "Technician\\CategoryController@edit");
-$router->put("/categorias/editar/{id}", "Technician\\CategoryController@update");
-$router->delete("categorias/excluir/{id}", "Technician\\CategoryController@destroy");
-
-//rotas das escolas
-$router->get("/escolas", "Technician\\SchoolController@index");
-$router->get("/escolas/cadastrar", "Technician\\SchoolController@create");
-$router->post("/escolas/cadastrar", "Technician\\SchoolController@store");
-$router->get("/escolas/editar/{id}", "Technician\\SchoolController@edit");
-$router->put("/escolas/editar/{id}", "Technician\\SchoolController@update");
-
-
-//rotas dos usuarios
-
-$router->get("/usuarios", "Technician\\UserController@index");
-$router->get("/usuarios/cadastrar", "Technician\\UserController@create");
-$router->post("/usuarios/cadastrar", "Technician\\UserController@store");
-$router->get("/usuarios/editar/{id}", "Technician\\UserController@edit");
-$router->put("/usuarios/editar/{id}", "Technician\\UserController@update");
 
 
 // rotas dos chamados
